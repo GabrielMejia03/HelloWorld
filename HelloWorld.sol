69c332a067520f390f88c5a863e2b4aefd29c124
GabrielMejia03 <112830347+GabrielMejia03@users.noreply.github.com>
2025-11-12 23:16:33 -0600

    HelloWorld

--- HelloWorld.sol
+++ HelloWorld.sol
@@ -3,7 +3,7 @@ pragma solidity ^0.8.12;
 
 contract Holamundo {
     mapping(address => string) private names;
-    mapping(address => address) private nameOwners;
+    mapping(address => address) private nameOwners;  // <-- Add this
 
     modifier onlyNameOwner() {
         require(
@@ -13,16 +13,16 @@ contract Holamundo {
         _;
     }
 
-    function storeName(string memory _name) external {
+    function GuardarNombre(string memory _name) external {
         names[msg.sender] = _name;
-        nameOwners[msg.sender] = msg.sender;
+        nameOwners[msg.sender] = msg.sender;  // <-- Track the owner
     }
 
-    function greet() external onlyNameOwner view returns (string memory) {
+    function Saludo() external onlyNameOwner view returns (string memory) {
         require(
             bytes(names[msg.sender]).length > 0,
             "No hay ningun nombre"
         );
-        return string(abi.encodePacked("Hello, ", names[msg.sender], "!"));
+        return string(abi.encodePacked("Saludos, ", names[msg.sender], "!"));
     }
 }
